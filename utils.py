c6bdec2a46f439e19273728464ff3555bf615492
Fauzan Aditia Pamungkas <fauzanpamungkas16jkt@gmail.com>
2023-03-09 10:01:01 +0700

    UPDATE CODE

--- utils.py
+++ utils.py
@@ -1,6 +1,16 @@
+from datetime import datetime
+from uuid import UUID, uuid4
+from pydantic import BaseModel, Field
 from starlette.responses import Response
 import typing
 import json
+
+
+class User(BaseModel):
+    id: UUID = Field(default_factory=uuid4)
+    name: str
+    joinDate: datetime = Field(default_factory=datetime.now)
+    isAdmin: bool = False
 
 
 class JSONResponse(Response):
